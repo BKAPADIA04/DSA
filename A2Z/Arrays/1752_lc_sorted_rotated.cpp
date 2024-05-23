a852a0eb9cc0d2794166841ae3bae84acdccff38
Bhavya <bkapadia04@gmail.com>
2024-05-24 00:55:54 +0530

    Commit

---
+++ A2Z/Arrays/1752_lc_sorted_rotated.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <iostream>
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int count = 0;
+        for(int i=0;i<nums.size()-1;i++) {
+            if(nums[i + 1] < nums[i])
+                count++;
+        }
+        if(nums[0] < nums[nums.size()-1]) {
+            count++;
+        }
+        return count <= 1;
+    }
+};
