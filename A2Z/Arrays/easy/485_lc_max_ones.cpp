99855e7a5ec1b12de691342454f9d9fa8a920e7b
Bhavya <bkapadia04@gmail.com>
2024-05-26 16:45:31 +0530

    Two Pointer

--- A2Z/Arrays/easy/485_lc_max_ones.cpp
+++ A2Z/Arrays/easy/485_lc_max_ones.cpp
@@ -17,6 +17,20 @@ public:
             }
         }
         return count;
+
+        // Alternative Approach
+        // int cnt = 0;
+        // int maxi = 0;
+        // for (int i = 0; i < nums.size(); i++) {
+        //     if (nums[i] == 1) {
+        //     cnt++;
+        //     } else {
+        //     cnt = 0;
+        //     }
+
+        //     maxi = max(maxi, cnt);
+        // }
+        // return maxi;
     }
 };
 int main() {}
