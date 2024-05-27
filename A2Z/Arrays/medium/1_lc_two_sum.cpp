462457675287ce55e2210fa5d4fbfca24a9ef90c
Bhavya <bkapadia04@gmail.com>
2024-05-27 13:24:08 +0530

    Map Hashing or Two Pointer

---
+++ A2Z/Arrays/medium/1_lc_two_sum.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        map<int,int>m;
+        for(int i = 0;i < n;i++) {
+            int rem = target - nums[i];
+            if(m.find(rem) != m.end()) {
+                return {m[rem],i};
+            }
+            m[nums[i]] = i;
+        }
+        return {-1,-1};
+    }
+};
