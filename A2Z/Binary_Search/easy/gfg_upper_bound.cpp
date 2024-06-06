75a0a6c87b28df3155086533dc8a57de002492d2
Bhavya <bkapadia04@gmail.com>
2024-06-06 17:04:55 +0530

    Upper Bound Implementation.

---
+++ A2Z/Binary_Search/easy/gfg_upper_bound.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findCeil(vector<long long> v, long long n, long long x){
+        
+    // Your code here
+    long long low = 0;
+    long long high = n - 1;
+    int index = n;
+    while(low <= high) {
+        long long mid = (low + high) / 2;
+        if(v[mid] > x) { index = mid; high = mid - 1; }
+        else  low = mid + 1;
+    }
+    return index;
+}
+
+int main() {
+    vector<long long>v = {1,2,8,10,11,12,19};
+    cout << findCeil(v,7,0) << endl;
+    cout << findCeil(v,7,19) << endl;
+    cout << findCeil(v,7,11) << endl;
+    vector<long long> numbers = {
+        66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130
+    };
+    cout << findCeil(numbers,65,106) << endl;
+}
