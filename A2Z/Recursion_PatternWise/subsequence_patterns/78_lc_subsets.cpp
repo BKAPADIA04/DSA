ddc883200923e87a3e1dc2b73b63a36acf6fc155
Bhavya <bkapadia04@gmail.com>
2024-07-01 13:08:12 +0530

    Subsets - Recursion

---
+++ A2Z/Recursion_PatternWise/subsequence_patterns/78_lc_subsets.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void generateSubsets(vector<vector<int>> &generate,vector<int>& nums,int i,int n) {
+    if(i == n) return;
+    int ele = nums[i];
+    for(int j = 0;j < generate.size();j++) {
+        vector<int>inner = generate[j];
+        if(count(inner.begin(),inner.end(),ele) == 0) {
+            inner.push_back(ele);
+            generate.push_back(inner);
+        }
+    }
+    generateSubsets(generate,nums,i+1,n);
+}
+
+vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>>generate;
+    generate.push_back({});
+    generateSubsets(generate,nums,0,nums.size());
+    return generate;  
+}
+
+int main() {
+    vector<int>nums = {1,2,3};
+    vector<vector<int>>answer = subsets(nums);
+    for(auto i:answer) {
+        for(auto j:i) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
