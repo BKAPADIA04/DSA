3d3632e13971451305722a74d6b42d804a57840d
Bhavya <bkapadia04@gmail.com>
2024-07-01 13:11:54 +0530

    Subsets - Recursion

--- A2Z/Recursion_PatternWise/subsequence_patterns/78_lc_subsets.cpp
+++ A2Z/Recursion_PatternWise/subsequence_patterns/78_lc_subsets.cpp
@@ -21,6 +21,20 @@ vector<vector<int>> subsets(vector<int>& nums) {
     return generate;  
 }
 
+// or
+void solve(int i, string s, string &f) {
+	if (i == s.length()) {
+		cout << f << " ";
+		return;
+	}
+	//picking 
+	f = f + s[i];
+	solve(i + 1, s,  f);
+	//poping out while backtracking
+	f.pop_back();
+	solve(i + 1, s,  f);
+}
+
 int main() {
     vector<int>nums = {1,2,3};
     vector<vector<int>>answer = subsets(nums);
