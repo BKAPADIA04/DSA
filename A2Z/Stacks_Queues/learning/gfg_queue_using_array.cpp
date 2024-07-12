06af05eb3f694d95c7f80c9ed23871f8d244af97
Bhavya <bkapadia04@gmail.com>
2024-07-12 13:11:46 +0530

    Implementation of Queue using Array

---
+++ A2Z/Stacks_Queues/learning/gfg_queue_using_array.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyQueue {
+private:
+    int arr[100005];
+    int front;
+    int rear;
+
+public :
+    MyQueue(){front=0;rear=0;}
+    void push(int);
+    int pop();
+};
+
+void MyQueue :: push(int x)
+{
+    arr[rear++] = x;
+}
+
+//Function to pop an element from queue and return that element.
+int MyQueue :: pop()
+{
+    if(front == rear) return -1;
+    int ele = arr[front++];
+    return ele;
+}
