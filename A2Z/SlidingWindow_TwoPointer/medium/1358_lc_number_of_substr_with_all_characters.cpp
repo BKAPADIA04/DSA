872e97d0e484438994c281867a9796b9c7a89cd8
Bhavya <bkapadia04@gmail.com>
2024-07-19 18:31:22 +0530

    Sliding Window Standad

---
+++ A2Z/SlidingWindow_TwoPointer/medium/1358_lc_number_of_substr_with_all_characters.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int numberOfSubstrings(string s) {
+    int n = s.size();
+    int count = 0;
+    vector<int>lastSeen(3,-1);
+    for(int i = 0;i < n;i++) {
+        lastSeen[s[i] - 'a'] = i;
+        if((lastSeen[0] != -1) && (lastSeen[1] != -1) && (lastSeen[2] != -1)) {
+            count += 1 + min(lastSeen[0],min(lastSeen[1],lastSeen[2]));
+        }
+    }        
+    return count;
+}
+
+int main() {
+    cout << numberOfSubstrings("abcabc") << endl;
+}
