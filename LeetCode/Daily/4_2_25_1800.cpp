6fc1be7a08deabcb8eef5ef56d8bd85a3b42798a
Bhavya Kapadia <bkapadia04@gmail.com>
2025-02-04 07:54:24 +0530

    Kadane

---
+++ LeetCode/Daily/4_2_25_1800.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int n = nums.size();
+        int ans = nums[0];
+        int sum = nums[0];
+        // for(int i = 1;i < n;i++) {
+        //     if(nums[i] <= nums[i-1]) {
+        //         sum = nums[i];
+        //     }
+        //     else {
+        //         sum += nums[i];
+        //     }
+        //     ans = max(ans,sum);
+        // }
+        for(int i = 1;i < n;i++) {
+            if(nums[i] <= nums[i-1]) {
+                sum = 0;
+            }
+            sum += nums[i];
+            ans = max(ans,sum);
+        }
+        return ans;
+    }
+};
