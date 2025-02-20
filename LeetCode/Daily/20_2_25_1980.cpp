9c03fbe45c463e9e6e9935ae0ffb9d7470b5fa88
Bhavya Kapadia <bkapadia04@gmail.com>
2025-02-20 08:41:06 +0530

    Random and Cantor Diag

---
+++ LeetCode/Daily/20_2_25_1980.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n = nums[0].size();
+        unordered_set<int>s;
+        for(int i = 0;i < nums.size();i++) {
+            s.insert(stoi(nums[i],0,2));
+        }
+
+        int rand_index = rand() % (nums.size());
+        int ans = stoi(nums[rand_index],0,2);
+
+        while(s.find(ans) != s.end()) {
+            ans = rand() % (int)pow(2,n);
+        }
+        return bitset<16>(ans).to_string().substr(16 - n);
+    }
+};
+
+/*class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        string ans;
+        for (int i = 0; i < nums.size(); i++) {
+            char curr = nums[i][i];
+            ans += curr == '0' ? '1' : '0';
+        }
+        
+        return ans;
+    }
+};*/
