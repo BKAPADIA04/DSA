f532fd72b5e375e9705c815bb8d5f5056bc2ed0b
Bhavya Kapadia <bkapadia04@gmail.com>
2025-03-23 12:04:11 +0530

    Prefix and Greedy

---
+++ LeetCode/Contests/3494_min_amount_time_brew_potatoes.cpp
@@ -0,0 +1,22 @@
+#define ll long long
+class Solution {
+public:
+    void print(vector<ll>v) {
+        for(auto it:v) cout << it << " ";
+        cout << endl;
+    }
+
+    long long minTime(vector<int>& skill, vector<int>& mana) {
+    vector<long long> endTime(skill.size(), 0);
+    for(int i = 0; i < mana.size(); ++i){
+        long long t = 0, maxDiff = 0;
+        for(int j = 0; j < skill.size(); ++j){
+            maxDiff = max(maxDiff, endTime[j] - t);
+            t += (long long)skill[j] * (long long)mana[i];
+            endTime[j] = t;
+        }
+        for(int j = 0; j < skill.size(); ++j) endTime[j] += maxDiff;
+    }
+    return endTime.back();
+}
+};
