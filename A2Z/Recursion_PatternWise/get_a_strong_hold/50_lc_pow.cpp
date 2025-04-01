7752ff33b90427a00cda7bf92cc7dbaa8d5166dd
Bhavya Kapadia <bkapadia04@gmail.com>
2025-04-01 08:20:35 +0530

    Update 50_lc_pow.cpp

--- A2Z/Recursion_PatternWise/get_a_strong_hold/50_lc_pow.cpp
+++ A2Z/Recursion_PatternWise/get_a_strong_hold/50_lc_pow.cpp
@@ -45,3 +45,24 @@ int main() {
     cout << myPow_(2,-3) << endl;
     cout << INT_MIN << " " << INT_MAX << endl;
 }
+
+
+class Solution {
+    public double myPow(double x, int n) {
+        if(n<0) {
+            return 1/apowb(x,n-2*n);
+        }
+        return apowb(x,n);
+        
+    }
+
+    public double apowb(double x,int y) {
+        if(y==0)
+        return 1d;
+        double power = (apowb(x,y/2));
+        if(y%2==0) {
+            return ((power)*(power));
+        }
+        return (x*(power*power));
+    }
+}
