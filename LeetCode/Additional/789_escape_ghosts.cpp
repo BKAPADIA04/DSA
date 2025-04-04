9c34f884170b9414b806791a739525f7ee0f1c02
Bhavya Kapadia <bkapadia04@gmail.com>
2025-04-04 15:20:08 +0530

    Math

---
+++ LeetCode/Additional/789_escape_ghosts.cpp
@@ -0,0 +1,14 @@
+#define ll long long
+class Solution {
+public:
+    bool escapeGhosts(vector<vector<int>>& ghosts, vector<int>& target) {
+        int n = ghosts.size();
+        ll chance = abs(target[0]) + abs(target[1]);
+
+        for(int i = 0;i < n;i++) {
+            ll temp = abs(ghosts[i][0] - target[0]) + abs(ghosts[i][1] - target[1]);
+            if(temp <= chance) return false;
+        }
+        return true;
+    }
+};
