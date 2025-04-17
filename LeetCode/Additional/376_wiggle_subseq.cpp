262fff7dd824e79843ef45c05300caca318d5361
Bhavya Kapadia <bkapadia04@gmail.com>
2025-04-17 12:30:31 +0530

    DP + Peak/Valley Greedy

--- LeetCode/Additional/376_wiggle_subseq.cpp
+++ LeetCode/Additional/376_wiggle_subseq.cpp
@@ -15,8 +15,7 @@ public:
         if(prev == -1) {
             take = max(take, 1 + check(nums,index + 1, index, tight));
         }
-
-        if(prev != -1) {
+        else {
             if(tight == 1) {
                 if(nums[index] < nums[prev]) {
                     take = max(take, 1 + check(nums, index + 1, index, 1 - tight));
@@ -46,4 +45,14 @@ public:
 
         return max(ans1,ans2);
     }   
-};
+};/*class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int size=nums.size(), peak=1, valley=1;
+        for(int i=1; i<size; ++i){
+                 if(nums[i]>nums[i-1]) peak = valley + 1;
+            else if(nums[i]<nums[i-1]) valley = peak + 1;
+        }
+        return max(peak , valley );
+    }
+};*/
