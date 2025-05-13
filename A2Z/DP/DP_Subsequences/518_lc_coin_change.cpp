f3b217ae33a7ff79a46892900bebb61bcecf8a22
Bhavya Kapadia <bkapadia04@gmail.com>
2025-05-13 15:56:40 +0530

    DP

--- A2Z/DP/DP_Subsequences/518_lc_coin_change.cpp
+++ A2Z/DP/DP_Subsequences/518_lc_coin_change.cpp
@@ -1,6 +1,51 @@
 class Solution {
 public:
 
+    int check(vector<int>&coins, int amount,vector<int>&dp) {
+        if(amount == 0) return 0;
+        if(dp[amount] != -1) return dp[amount];
+        int ans = 1e9;
+        for(int i = 0;i < coins.size();i++) {
+            if(coins[i] <= amount) {
+                ans = min(ans,1 + check(coins,amount - coins[i],dp));
+            }
+        }
+        return dp[amount] = ans;
+    }
+
+
+    int coinChange(vector<int>& coins, int amount) {
+        if(amount == 0) return 0;
+        vector<int>dp(amount + 1,amount + 1);
+        dp[0] = 0;
+        for(int coin:coins) {
+            for(int i = coin; i <= amount;i++) {
+                dp[i] = min(dp[i],dp[i-coin] + 1);
+            }
+        }
+        // return dp[amount] == amount + 1 ? -1 : dp[amount];
+        vector<int>dp_rec(amount + 1,-1);
+        int ans = check(coins,amount,dp_rec);
+        // return ans == 1e9 ? -1 : ans;
+
+
+        vector<int>dp_tab(amount+1,amount+1);
+        dp_tab[0] = 0;
+        for(int i = 1;i <= amount;i++) {
+            for(int j = 0;j < coins.size();j++) {
+                int temp = 1e9;
+                if(coins[j] <= i) temp = 1 + dp_tab[i - coins[j]];
+                dp_tab[i] = min(dp_tab[i],temp);
+            }
+        }
+        return dp_tab[amount] == amount + 1 ? -1 : dp_tab[amount];
+    }
+};
+
+
+class Solution {
+public:
+
     int check(int amount,vector<int>& coins,int index,vector<vector<int>>&dp) {
         if (index == 0) {
             // Check if the target sum is divisible by the first element
@@ -24,3 +69,4 @@ public:
         return check(amount,coins,coins.size()-1,dp);
     }
 };
+
