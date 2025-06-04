cb18bf82be28378667818e04e55f2a1b495ea320
Bhavya Kapadia <bkapadia04@gmail.com>
2025-06-04 08:15:00 +0530

    Two Pointers

---
+++ LeetCode/Hards/SlidingWindow/1163_last_substring.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string lastSubstring(string s) {
+        int n = s.size();
+        int i = 0; int j = 1; int k = 0;
+        while(j + k < n) {
+            if(s[i + k] == s[j + k]) { k++; continue;}
+            if(s[i + k] > s[j + k]) j = j + k + 1;
+            else {
+                i = max(i + k + 1,j);
+                j = i + 1;
+            }
+            k = 0;
+        }
+        return s.substr(i);
+    }
+};
