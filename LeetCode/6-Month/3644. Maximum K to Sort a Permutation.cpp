e6dd68b2769015b096b75ee25527cab34b7a2ec2
Bhavya Kapadia <bkapadia04@gmail.com>
2026-02-13 09:45:53 +0530

    Bit Manipulation

---
+++ LeetCode/6-Month/3644. Maximum K to Sort a Permutation.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int sortPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int ans = INT_MAX;
+        for(int i = 0;i < n;i++) {
+            if(nums[i] == i) continue;
+            ans &= nums[i];
+        }
+
+        if(ans == INT_MAX) return 0;
+        return ans;
+    }
+};
