36ad9fffac6f7a432f6b5e9d17e5febf2331bbea
Bhavya Kapadia <bkapadia04@gmail.com>
2026-02-23 19:42:46 +0530

    Hea[s
    Implement a solution to find the minimum array length after pair removals.

---
+++ LeetCode/6-Month/2856. Minimum Array Length After Pair Removals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minLengthAfterRemovals(vector<int>& nums) {
+        map<int,int>mp;
+        for(auto it:nums) mp[it]++;
+        priority_queue<int>pq;
+        for(auto it:mp) pq.push(it.second);
+
+        while(pq.size() >= 2) {
+            int num1 = pq.top();
+            pq.pop();
+            int num2 = pq.top();
+            pq.pop();
+            num1--;
+            num2--;
+            if(num1 > 0) pq.push(num1);
+            if(num2 > 0) pq.push(num2); 
+        }
+
+        return pq.size() ? pq.top() : 0;
+    }
+};
