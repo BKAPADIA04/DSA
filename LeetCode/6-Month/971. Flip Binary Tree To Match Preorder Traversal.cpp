77c14459d22ee67344ca8f3e35fb8dcf56db012a
Bhavya Kapadia <bkapadia04@gmail.com>
2026-04-23 11:00:00 +0530

    Standard Post Order DFSD

---
+++ LeetCode/6-Month/971. Flip Binary Tree To Match Preorder Traversal.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int i;
+    vector<int>answer;
+    bool check(TreeNode * root, vector<int>&voyage) {
+        if(root == nullptr) return true;
+        if(root->val != voyage[i++]) return false;
+
+        if(root->left && root->left->val != voyage[i]) {
+            answer.push_back(root->val);
+
+            return check(root->right, voyage) && check(root->left, voyage);
+        }
+
+        return check(root->left, voyage) && check(root->right, voyage);
+    }
+
+    vector<int> flipMatchVoyage(TreeNode* root, vector<int>& voyage) {
+        i = 0;
+        bool ans = check(root, voyage);
+        if(!ans) return {-1};
+        return answer;
+    }
+};
